a0ad2d3d6ee1a291387692e376193f2d400896a0
Sam Glasby <sglasby@cableone.net>
2012-09-16 13:50:27 -0600

    Changed LockBits() parameters for WinXP

--- OpenGL_Transparent_Sprite.cs
+++ OpenGL_Transparent_Sprite.cs
@@ -8,13 +8,17 @@ using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 
 public class AppWindow : GameWindow {
-    int texture;
+    int texture_reticle;
+    int texture_ship;
     float x, y;
 
+    const int TILE_W = 32;
+    const int TILE_H = 32;
+
     public AppWindow() {
-        this.Title = "OpenTK App 2";
+        this.Title        = "OpenTK App 2";
         this.WindowBorder = WindowBorder.Fixed;
-        this.ClientSize = new Size(800, 600);
+        this.ClientSize   = new Size(800, 600);
     }
 
     protected override void OnLoad(EventArgs e) {
@@ -27,26 +31,42 @@ public class AppWindow : GameWindow {
         GL.Enable(EnableCap.Texture2D);
         GL.Enable(EnableCap.Blend);
         GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
-
         GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
 
-        GL.GenTextures(1, out this.texture);
-        GL.BindTexture(TextureTarget.Texture2D, this.texture);
+        this.texture_reticle = Load_Texture(@"bright_marquee.frame_1.png", 3, 3);
+        this.texture_ship    = Load_Texture(@"U4.B_enhanced-32x32.png",    2, 1);
 
-        //Bitmap bitmap = new Bitmap("bright_marquee.frame_1.png");  // Transparency works on this image -- my transparency problem is NOT with this image file
-        Bitmap bitmap = new Bitmap("frame1.png");  // Transparency works on this image
+        this.x = 1 + TILE_W/2;
+        this.y = 1 + TILE_H/2;
+    } // OnLoad()
+
+    protected int Load_Texture(string filename, int xx, int yy) {
+        int texture_id;
+
+        Bitmap bitmap = new Bitmap(filename);
         bitmap.MakeTransparent(Color.Magenta);
 
-        Rectangle  rect = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+        int px_x = (xx * TILE_W);
+        int px_y = (yy * TILE_H);
+
+        GL.GenTextures(1, out texture_id);
+        GL.BindTexture(TextureTarget.Texture2D, texture_id);
+
+        Rectangle  rect = new Rectangle(px_x, px_y, TILE_W, TILE_H);
         BitmapData data = bitmap.LockBits(rect,
                                           ImageLockMode.ReadOnly, 
-                                          System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                                          System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+                                          //System.Drawing.Imaging.PixelFormat.Format32bppRgb);  // This value worked on Windows 7 ???
 
         GL.TexImage2D(
-            OpenTK.Graphics.OpenGL.TextureTarget.Texture2D, 0,         // texture_target, level
-            OpenTK.Graphics.OpenGL.PixelInternalFormat.Rgba, data.Width, data.Height, 0,  // internal_format, width, height, border
-            OpenTK.Graphics.OpenGL.PixelFormat.Bgra,                   // pixel_format
-            OpenTK.Graphics.OpenGL.PixelType.UnsignedByte, data.Scan0  // pixel_type, pixels
+            OpenTK.Graphics.OpenGL.TextureTarget.Texture2D,   // texture_target,
+            0,                                                // level,
+            OpenTK.Graphics.OpenGL.PixelInternalFormat.Rgba,  // internal_format
+            data.Width, data.Height,                          // width, height, 
+            0,                                                // border,
+            OpenTK.Graphics.OpenGL.PixelFormat.Bgra,          // pixel_format
+            OpenTK.Graphics.OpenGL.PixelType.UnsignedByte,    // pixel_type
+            data.Scan0                                        // pixels
             );
 
         bitmap.UnlockBits(data);
@@ -55,9 +75,8 @@ public class AppWindow : GameWindow {
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int) TextureMinFilter.Linear);
         GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int) TextureMagFilter.Linear);
 
-        this.x = 40f;
-        this.y = 41.5f;
-    }
+        return texture_id;
+    } // Load_Texture()
 
     protected override void OnUpdateFrame(FrameEventArgs e) {
         base.OnUpdateFrame(e);
@@ -82,26 +101,34 @@ public class AppWindow : GameWindow {
     protected override void OnRenderFrame(FrameEventArgs e) {
         base.OnRenderFrame(e);
 
+        int origin_x = 1 + TILE_W;
+        int origin_y = 1 + TILE_H;
+
         GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
 
         GL.MatrixMode(MatrixMode.Projection);
         GL.LoadIdentity();
 
+        GL.BindTexture(TextureTarget.Texture2D, this.texture_ship);
         GL.Begin(BeginMode.Quads);
-
-        GL.TexCoord2(0, 0);  GL.Vertex2(200 - 64.0f, 200 - 64.0f);
-        GL.TexCoord2(1, 0);  GL.Vertex2(200 + 64.0f, 200 - 64.0f);
-        GL.TexCoord2(1, 1);  GL.Vertex2(200 + 64.0f, 200 + 64.0f);
-        GL.TexCoord2(0, 1);  GL.Vertex2(200 - 64.0f, 200 + 64.0f);
-
-        GL.TexCoord2(0, 0);  GL.Vertex2(this.x - 64.0f, this.y - 64.0f);
-        GL.TexCoord2(1, 0);  GL.Vertex2(this.x + 64.0f, this.y - 64.0f);
-        GL.TexCoord2(1, 1);  GL.Vertex2(this.x + 64.0f, this.y + 64.0f);
-        GL.TexCoord2(0, 1);  GL.Vertex2(this.x - 64.0f, this.y + 64.0f);
-
+        {
+            GL.TexCoord2(0, 0);  GL.Vertex2(origin_x - TILE_W/2, origin_y - TILE_H/2);
+            GL.TexCoord2(1, 0);  GL.Vertex2(origin_x + TILE_W/2, origin_y - TILE_H/2);
+            GL.TexCoord2(1, 1);  GL.Vertex2(origin_x + TILE_W/2, origin_y + TILE_H/2);
+            GL.TexCoord2(0, 1);  GL.Vertex2(origin_x - TILE_W/2, origin_y + TILE_H/2);
+        }
         GL.End();
 
-        //GL.Flush();  // Not useful, according to one Stack Overflow OpenGL person
+        GL.BindTexture(TextureTarget.Texture2D, this.texture_reticle);
+        GL.Begin(BeginMode.Quads);
+        {
+            GL.TexCoord2(0, 0);  GL.Vertex2(this.x - TILE_W/2, this.y - TILE_H/2);
+            GL.TexCoord2(1, 0);  GL.Vertex2(this.x + TILE_W/2, this.y - TILE_H/2);
+            GL.TexCoord2(1, 1);  GL.Vertex2(this.x + TILE_W/2, this.y + TILE_H/2);
+            GL.TexCoord2(0, 1);  GL.Vertex2(this.x - TILE_W/2, this.y + TILE_H/2);
+        }
+        GL.End();
+
         this.SwapBuffers();
     }
 
