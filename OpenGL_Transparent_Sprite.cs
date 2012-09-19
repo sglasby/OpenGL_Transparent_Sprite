7c62d149f26fcf658590514bd58942afe1b5e1e7
Sam Glasby <sglasby@cableone.net>
2012-09-18 21:59:25 -0600

    Added GL error checking, Format32bppArgb
     to ill
    
    Added GL error checking of a sort.
    Calling LockBits() with Format32bppArgb to illustrate the scrambled
    result.
    (Uncomment the line with Format32bppPArgb for the "working" result in my
    text systems.)
    Added a couple of screen shots.

--- OpenGL_Transparent_Sprite.cs
+++ OpenGL_Transparent_Sprite.cs
@@ -32,6 +32,7 @@ public class AppWindow : GameWindow {
         GL.Enable(EnableCap.Blend);
         GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
         GL.Hint(HintTarget.PerspectiveCorrectionHint, HintMode.Nicest);
+        Check_for_GL_error("After calling GL.Hint()");
 
         this.texture_reticle = Load_Texture(@"bright_marquee.frame_1.png", 3, 3);
         this.texture_ship    = Load_Texture(@"U4.B_enhanced-32x32.png",    2, 1);
@@ -51,12 +52,16 @@ public class AppWindow : GameWindow {
 
         GL.GenTextures(1, out texture_id);
         GL.BindTexture(TextureTarget.Texture2D, texture_id);
+        Check_for_GL_error("After calling GL.BindTexture()");
 
         Rectangle  rect = new Rectangle(px_x, px_y, TILE_W, TILE_H);
         BitmapData data = bitmap.LockBits(rect,
                                           ImageLockMode.ReadOnly, 
-                                          System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
-                                          //System.Drawing.Imaging.PixelFormat.Format32bppRgb);  // This value worked on Windows 7 ???
+                                          //System.Drawing.Imaging.PixelFormat.Format32bppPArgb);  // With this line uncommented, it works on XP and Ubuntu
+                                          //System.Drawing.Imaging.PixelFormat.Format32bppRgb);  // This value worked on Windows 7, but on XP I see black pixels rather than transparent
+                                          System.Drawing.Imaging.PixelFormat.Format32bppArgb);  // With this line uncommented, I get "scrambled" results on XP
+        // Format32bppArgb was indicated by OpenTK.com user "Inertia", citing http://msdn.microsoft.com/en-us/library/8517ckds.aspx
+        // However, rather than transparency, I observe a strange display (sending a screen shot)
 
         GL.TexImage2D(
             OpenTK.Graphics.OpenGL.TextureTarget.Texture2D,   // texture_target,
@@ -68,6 +73,7 @@ public class AppWindow : GameWindow {
             OpenTK.Graphics.OpenGL.PixelType.UnsignedByte,    // pixel_type
             data.Scan0                                        // pixels
             );
+        Check_for_GL_error("After calling GL.TexImage2D()");
 
         bitmap.UnlockBits(data);
         bitmap.Dispose();
@@ -110,6 +116,8 @@ public class AppWindow : GameWindow {
         GL.LoadIdentity();
 
         GL.BindTexture(TextureTarget.Texture2D, this.texture_ship);
+        Check_for_GL_error("In OnRenderFrame(), after 1st call to GL.BindTexture()");
+
         GL.Begin(BeginMode.Quads);
         {
             GL.TexCoord2(0, 0);  GL.Vertex2(origin_x - TILE_W/2, origin_y - TILE_H/2);
@@ -120,6 +128,7 @@ public class AppWindow : GameWindow {
         GL.End();
 
         GL.BindTexture(TextureTarget.Texture2D, this.texture_reticle);
+        Check_for_GL_error("In OnRenderFrame(), after 2nd call to GL.BindTexture()");
         GL.Begin(BeginMode.Quads);
         {
             GL.TexCoord2(0, 0);  GL.Vertex2(this.x - TILE_W/2, this.y - TILE_H/2);
@@ -132,9 +141,18 @@ public class AppWindow : GameWindow {
         this.SwapBuffers();
     }
 
+    public static void Check_for_GL_error(string msg) {
+        ErrorCode err = GL.GetError();
+        if (err != ErrorCode.NoError) {
+            string str = String.Format("{0} GL.GetError() returns: {1}", msg, err);
+            throw new Exception(str);
+        }
+    } // Check_for_GL_error()
+
     [STAThread]
     public static void Main() {
         AppWindow window = new AppWindow();
         window.Run(60);
     }
+
 } // class
